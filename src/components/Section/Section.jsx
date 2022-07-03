5b128f69f4e3d5f2cd79ae86cf9e4b0521d99427
Elena S <76928777+helenserdiuk@users.noreply.github.com>
2022-07-04 02:52:41 +0300

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
 
 const Section = ({ title, children }) => {
+  return (
   <div>
     <h2>{title}</h2>
     {children}
-  </div>;
+  </div>
+)
 };
 
 Section.propTypes = {
